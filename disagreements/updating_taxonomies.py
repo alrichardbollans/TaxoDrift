51781e75f22e0f85febefb291a1615ad0ea05bee
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-08-07 11:58:28 +0100

    Add more results

--- disagreements/updating_taxonomies.py
+++ disagreements/updating_taxonomies.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 from pkg_resources import resource_filename
 from wcvpy.wcvp_download import get_all_taxa, add_authors_to_col
+from wcvpy.wcvp_name_matching import get_genus_from_full_name
 
 _output_path = resource_filename(__name__, 'outputs')
 
@@ -28,21 +29,18 @@ def get_accepted_name_from_record(record: pd.DataFrame, reported_name: str):
             return None
 
 
-def compare_two_versions(v12_taxa: pd.DataFrame, v13_taxa: pd.DataFrame, out_dir: str):
-    # For all taxa with author strings in old taxon database
-    # If the name resolves to a non-nan accepted name in both the old and new database
-    # Find the accepted name resolution when the name is resolved first to the old taxonnomy then the new taxonomy
+def compare_two_versions(v12_taxa: pd.DataFrame, v13_taxa: pd.DataFrame, old_tag: str, new_tag: str):
+    # For all taxa with unique names (inc. author strings) in old taxon database
+    # If the name resolves uniquely to a non-nan accepted name in both the old and new database
+    # Find the accepted name resolution when the name is resolved first to the old taxonomy then the new taxonomy
     # If no such name exists, add to cases_that_cant_update
     # else, check if this name is the same as the name when directly resolved using the new database. If not, store in out_dict
-
+    out_dir = os.path.join(_output_path,
+                           '_'.join([old_tag, new_tag]))
     os.makedirs(out_dir, exist_ok=True)
 
     v12_taxa['taxon_name_w_authors'] = add_authors_to_col(v12_taxa, 'taxon_name')
     v13_taxa['taxon_name_w_authors'] = add_authors_to_col(v13_taxa, 'taxon_name')
-    # out_dict = {'reported_name': [], 'v12_accepted_name': [], 'v13_accepted_name': [], 'v12_accepted_name_in_v13': []}
-    # cases_that_cant_update = {'reported_name': [], 'v12_accepted_name': [], 'v13_accepted_name': []}
-    # cases_that_cant_update_because_of_multiples = {'reported_name': [], 'v12_accepted_name': [], 'v13_accepted_name': []}
-    # names_in_old_with_multiple_resolutions = []
 
     # Pick non-duplicated names
     unique_names = v12_taxa['taxon_name_w_authors'].dropna().unique().tolist()
@@ -51,100 +49,94 @@ def compare_two_versions(v12_taxa: pd.DataFrame, v13_taxa: pd.DataFrame, out_dir
     # relevant records
     v12_records = v12_taxa[v12_taxa['taxon_name_w_authors'].isin(unique_names)][
         ['taxon_name_w_authors', 'accepted_name_w_author']]
+    v12_records = v12_records.dropna(subset=['accepted_name_w_author'])
     v12_records = v12_records.drop_duplicates(keep='first')
     v12_records = v12_records.drop_duplicates(subset=['taxon_name_w_authors'],
                                               keep=False)  # Ignore cases with multiple resolutions, as these are ambiguous anyway
+    # I think Cubeba Raf. and Lamottea Pomel are examples but they are less interesting for this analysis
+
+    v12_records = v12_records.rename(
+        columns={'accepted_name_w_author': old_tag + '_accepted_name_w_author'})
+    v12_records.describe(include='all').to_csv(os.path.join(out_dir, 'old_records_summary.csv'))
+
     # accepted names in old database
-    relevant_v13_names = v12_records['accepted_name_w_author'].dropna().unique().tolist()
+    relevant_v13_names_for_chaining = v12_records[old_tag + '_accepted_name_w_author'].dropna().unique().tolist()
     # names in new database where taxon name is accepted name in old database
-    v13_records_for_chaining = v13_taxa[v13_taxa['taxon_name_w_authors'].isin(relevant_v13_names)][
-        ['taxon_name_w_authors', 'accepted_name_w_author']]
+    v13_records_for_chaining = v13_taxa[v13_taxa['taxon_name_w_authors'].isin(relevant_v13_names_for_chaining)][
+        ['taxon_name_w_authors', 'accepted_name_w_author', 'accepted_species_w_author']]
     v13_records_for_chaining = v13_records_for_chaining.drop_duplicates(keep='first')
     v13_records_for_chaining = v13_records_for_chaining.drop_duplicates(subset=['taxon_name_w_authors'],
                                                                         keep=False)  # Ignore cases with multiple resolutions, as these are ambiguous anyway
     v13_records_for_chaining = v13_records_for_chaining.rename(
-        columns={'taxon_name_w_authors': 'v13_taxon_name_w_authors', 'accepted_name_w_author': 'v13_chained_accepted_name_w_author'})
+        columns={'taxon_name_w_authors': new_tag + '_taxon_name_w_authors', 'accepted_name_w_author': new_tag + '_chained_accepted_name_w_author',
+                 'accepted_species_w_author': new_tag + '_chained_accepted_species_w_author'})
     # chained result where taxon_name_w_authors are resolved to v13_chained_accepted_name_w_author, mediated by old database
-    chained_updated_records = pd.merge(v12_records, v13_records_for_chaining, left_on='accepted_name_w_author',
-                                       right_on='v13_taxon_name_w_authors')
+    chained_updated_records = pd.merge(v12_records, v13_records_for_chaining, left_on=old_tag + '_accepted_name_w_author',
+                                       right_on=new_tag + '_taxon_name_w_authors')
+    chained_updated_records.describe(include='all').to_csv(os.path.join(out_dir, 'chained_updated_records_summary.csv'))
 
     # relevant names in new database where taxon name is taxon name in old database
-    v13_updated_records = v13_taxa[v13_taxa['taxon_name_w_authors'].isin(unique_names)][['taxon_name_w_authors', 'accepted_name_w_author']]
+    v13_updated_records = v13_taxa[v13_taxa['taxon_name_w_authors'].isin(unique_names)][
+        ['taxon_name_w_authors', 'accepted_name_w_author', 'accepted_species_w_author']]
+    v13_updated_records = v13_updated_records.dropna(subset=['accepted_name_w_author'])
     v13_updated_records = v13_updated_records.drop_duplicates(keep='first')
     v13_updated_records = v13_updated_records.drop_duplicates(subset=['taxon_name_w_authors'],
                                                               keep=False)  # Ignore cases with multiple resolutions, as these are ambiguous anyway
     v13_updated_records = v13_updated_records.rename(
-        columns={'accepted_name_w_author': 'v13_direct_accepted_name_w_author'})
+        columns={'accepted_name_w_author': new_tag + '_direct_accepted_name_w_author',
+                 'accepted_species_w_author': new_tag + '_direct_accepted_species_w_author'})
+    v13_updated_records.describe(include='all').to_csv(os.path.join(out_dir, 'direct_updated_records_summary.csv'))
 
     # direct result where taxon_name_w_authors are resolved directly to the new databse
     merged_df = pd.merge(chained_updated_records, v13_updated_records, on='taxon_name_w_authors')
 
-    results_df = merged_df[merged_df['v13_direct_accepted_name_w_author'] != merged_df['v13_chained_accepted_name_w_author']]
-    results_df = results_df.dropna(subset=['v13_direct_accepted_name_w_author'])
-    # TODO: add rank info
-    # Cases like Cubeba Raf. and Lamottea Pomel are only here because of the multiple resolutions -- which is interesting in itself
+    results_df = merged_df[merged_df[new_tag + '_direct_accepted_name_w_author'] != merged_df[new_tag + '_chained_accepted_name_w_author']]
+    results_df = results_df.dropna(subset=[new_tag + '_direct_accepted_name_w_author'])
 
-    # for reported_name in tqdm(unique_names):
-    #
-    #     v12_record = v12_records[v12_records['taxon_name_w_authors'] == reported_name]
-    #     try:
-    #         v12_accepted_name_w_author = get_accepted_name_from_record(v12_record, reported_name)
-    #         if v12_accepted_name_w_author is not None:  # If there is a resolution to an accepted name
-    #             v13_record = v13_records[v13_records['taxon_name_w_authors'] == reported_name]
-    #             if len(v13_record.index) > 0:  # Only consider cases where the reported name is in both taxonomies
-    #                 v13_accepted_name_w_author = get_accepted_name_from_record(v13_record, reported_name)
-    #
-    #                 v12_accepted_name_updated_record = v13_records[v13_records['taxon_name_w_authors'] == v12_accepted_name_w_author]
-    #                 if len(v12_accepted_name_updated_record.index) == 0:
-    #                     cases_that_cant_update['reported_name'].append(reported_name)
-    #                     cases_that_cant_update['v12_accepted_name'].append(v12_accepted_name_w_author)
-    #                     cases_that_cant_update['v13_accepted_name'].append(v13_accepted_name_w_author)
-    #
-    #                 else:
-    #                     # Name Diplazium lonchophyllum Kunze resolves to two different taxa!
-    #                     try:
-    #                         v12_updated_accepted_name = get_accepted_name_from_record(v12_accepted_name_updated_record, reported_name)
-    #                         if v12_updated_accepted_name != v13_accepted_name_w_author:
-    #                             print('Found one!')
-    #                             out_dict['reported_name'].append(reported_name)
-    #                             out_dict['v12_accepted_name'].append(v12_accepted_name_w_author)
-    #                             out_dict['v13_accepted_name'].append(v13_accepted_name_w_author)
-    #                             out_dict['v12_accepted_name_in_v13'].append(v12_updated_accepted_name)
-    #                             print(out_dict)
-    #                     except ValueError:
-    #                         cases_that_cant_update_because_of_multiples['reported_name'].append(reported_name)
-    #                         cases_that_cant_update_because_of_multiples['v12_accepted_name'].append(v12_accepted_name_w_author)
-    #                         cases_that_cant_update_because_of_multiples['v13_accepted_name'].append(v13_accepted_name_w_author)
-    #     except:
-    #         names_in_old_with_multiple_resolutions.append(reported_name)
+    # Add longer chains
 
-    # print(out_dict)
-    # out_df = pd.DataFrame.from_dict(out_dict)
-    # cases_that_cant_update_df = pd.DataFrame.from_dict(cases_that_cant_update)
-    # names_in_old_with_multiple_resolutions_df = pd.DataFrame.from_dict(
-    #     {'names_in_old_with_multiple_resolutions': names_in_old_with_multiple_resolutions})
+    results_df.to_csv(os.path.join(out_dir, 'all_results.csv'))
+    results_df.describe(include='all').to_csv(os.path.join(out_dir, 'all_results_summary.csv'))
 
-    results_df.to_csv(os.path.join(out_dir, 'results.csv'))
+    species_ambiguity_results = results_df.dropna(subset=[new_tag + '_direct_accepted_species_w_author'])
+    species_ambiguity_results = species_ambiguity_results[
+        species_ambiguity_results[new_tag + '_direct_accepted_species_w_author'] != species_ambiguity_results[
+            new_tag + '_chained_accepted_species_w_author']]
+
+    species_ambiguity_results.to_csv(os.path.join(out_dir, 'species_results.csv'))
+    species_ambiguity_results.describe(include='all').to_csv(os.path.join(out_dir, 'species_results_summary.csv'))
+
+    species_ambiguity_results[new_tag + '_chained_accepted_genus'] = species_ambiguity_results[new_tag + '_chained_accepted_species_w_author'].apply(
+        get_genus_from_full_name)
+    species_ambiguity_results[new_tag + '_direct_accepted_genus'] = species_ambiguity_results[new_tag + '_direct_accepted_species_w_author'].apply(
+        get_genus_from_full_name)
+
+    genus_ambiguity_results = species_ambiguity_results.dropna(subset=[new_tag + '_direct_accepted_genus'])
+    genus_ambiguity_results = genus_ambiguity_results[
+        genus_ambiguity_results[new_tag + '_chained_accepted_genus'] != genus_ambiguity_results[new_tag + '_direct_accepted_genus']]
+    genus_ambiguity_results.to_csv(os.path.join(out_dir, 'genus_results.csv'))
+    genus_ambiguity_results.describe(include='all').to_csv(os.path.join(out_dir, 'genus_results_summary.csv'))
     # cases_that_cant_update_df.to_csv(os.path.join(out_dir, 'v12_v13_cases_cant_update.csv'))
     # names_in_old_with_multiple_resolutions_df.to_csv(os.path.join(out_dir, 'v12_v13_names_in_old_with_multiple_resolutions.csv'))
 
-    return results_df  # , cases_that_cant_update_df, names_in_old_with_multiple_resolutions_df
+    return results_df
 
 
 def main():
     v12_taxa = get_all_taxa(version='12')
     v13_taxa = get_all_taxa(version=None)
-    compare_two_versions(v12_taxa, v13_taxa,
-                         os.path.join(_output_path,
-                                      'v12_v13'))
+    compare_two_versions(v12_taxa, v13_taxa, 'v12', 'v13')
     v10_taxa = get_all_taxa(version='10')
     compare_two_versions(v10_taxa, v13_taxa,
-                         os.path.join(_output_path,
-                                      'v10_v13'))
+                         'v10', 'v13')
     v11_taxa = get_all_taxa(version='11')
     compare_two_versions(v11_taxa, v13_taxa,
-                         os.path.join(_output_path,
-                                      'v11_v13'))
+                         'v11', 'v13')
+
+    compare_two_versions(v10_taxa, v11_taxa,
+                         'v10', 'v11')
+    compare_two_versions(v11_taxa, v12_taxa,
+                         'v11', 'v12')
 
 
 if __name__ == '__main__':
